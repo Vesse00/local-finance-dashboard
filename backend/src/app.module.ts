8f961a632d04c2c18349e0b183d7e021ee569358
Vesse00 <55257923+Vesse00@users.noreply.github.com>
2026-04-24 22:50:19 +0200

    migrate backend to NESTJS

---
+++ backend/src/app.module.ts
@@ -0,0 +1,5 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { SavingsModule } from './savings/savings.module';
+import { TransferModule } from './transfer/transfer.module';
